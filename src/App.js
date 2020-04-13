1ce22adc6937754000bc2dd64b4eb566546623ad
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-04-13 18:56:59 +0200

    Added basic map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WaysToGo</h1>
-      <p>In construction</p>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    lat: 48.856614,
+    lng: 2.3522219,
+    zoom: 12,
+  };
 
-export default App;
+  getLogoStyle = () => {
+    return {
+      position: "absolute",
+      zIndex: 1,
+      left: "40px",
+    };
+  };
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <div className="App">
+        <h1 style={this.getLogoStyle()}>WaysToGo</h1>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </Map>
+      </div>
+    );
+  }
+}
