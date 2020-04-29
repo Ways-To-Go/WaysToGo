a55ab8b80bfc1d070be8fca037b7f0a4f6f6eeca
leon-dav <l.davidovski1@gmail.com>
2020-04-29 23:52:10 +0200

    carte pointeur interactif

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./App.css";
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 
 class ShowVoyage extends Component {
@@ -73,44 +74,32 @@ class ShowVoyage extends Component {
 }
 
 
+
 class ShowCarte extends Component {
-  state = {
-    lat: 48.856614,
-    lng: 2.3522219,
-    zoom: 2,
-    cities: [
-      {
-        name: "Paris",
-        trips: [
-          {
-            id: 1,
-            city: "Paris",
-            title: "My awesome trip",
-            desc: "Some info about my awesome trip!",
-          },
-          {
-            id: 3,
-            city: "Paris",
-            title: "Romance in Paris",
-            desc: "Cool trip in Paris",
-          },
-        ],
-        coordinates: [48.856614, 2.3522219],
-      },
-      {
-        name: "Madrid",
-        trips: [
-          {
-            id: 2,
-            city: "Madrid",
-            title: "Adventure in Madrid",
-            desc: "some info",
-          },
-        ],
-        coordinates: [37.7167, -2.1417],
-      },
-    ],
-  };
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+		  liste_voyage: [],
+		  villes: ["Paris", "Madrid"],
+
+		  lat: 48.856614,
+		  lng: 2.3522219,
+		  zoom: 2
+		};
+		
+		const provider = new OpenStreetMapProvider();
+		{this.state.villes.map((city) => (
+		  provider
+			  .search({ query: city })
+			  .then((result) => (
+			  this.setState({ liste_voyage : this.state.liste_voyage.concat([result, city])})
+		  ))
+	  ))};
+	  
+	}
+	
+  
 
   getLogoStyle = () => {
     return {
@@ -121,34 +110,27 @@ class ShowCarte extends Component {
   };
 
   render() {
-    const position = [this.state.lat, this.state.lng];
     return (
       <div className="App">
         <h1 style={this.getLogoStyle()}>WaysToGo</h1>
-        <Map center={position} zoom={this.state.zoom}>
+        <Map center={[this.state.lat, this.state.lng]} zoom={this.state.zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {this.state.cities.map((city) => (
-            <Marker position={city.coordinates}>
+          {this.state.liste_voyage.map((coord, city) => (
+			  coord[0].x != undefined ?
+		  <Marker position={[coord[0].y, coord[0].x]}>
+		  console.log(coord);
               <Popup>
-                <h1>{city.name}</h1>
-                {city.trips.map((trip) => (
-                  <div>
-                    <h3>
-                      <a href={"?show=trip&id=" + trip.id}>{trip.title}</a>
-                    </h3>
-                    <p>{trip.desc}</p>
-                  </div>
-                ))}
+                <h1>{city}</h1>
               </Popup>
-            </Marker>
+			  </Marker> : ""
           ))}
         </Map>
       </div>
-    );
+	  );
   }
 }
 
