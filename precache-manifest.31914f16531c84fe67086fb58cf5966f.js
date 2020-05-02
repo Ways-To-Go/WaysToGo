37d54219377747efad860b7bdf921c0cd5d04b31
leon-dav <l.davidovski1@gmail.com>
2020-05-02 10:58:55 +0200

    Updates

---
+++ precache-manifest.31914f16531c84fe67086fb58cf5966f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbb2a3de5b3ecd94dd19b3e2e5b4f02",
+    "url": "/WaysToGo/index.html"
+  },
+  {
+    "revision": "a2dc6764c0249587b4c0",
+    "url": "/WaysToGo/static/css/main.b9836877.chunk.css"
+  },
+  {
+    "revision": "5103b8b54ffb96146bb9",
+    "url": "/WaysToGo/static/js/2.fd74f60b.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/WaysToGo/static/js/2.fd74f60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2dc6764c0249587b4c0",
+    "url": "/WaysToGo/static/js/main.bd83bcd0.chunk.js"
+  },
+  {
+    "revision": "f200946a1c0a8cd0965b",
+    "url": "/WaysToGo/static/js/runtime-main.29cc8039.js"
+  }
+]);
