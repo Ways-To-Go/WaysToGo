f05c79b080c0e832733ec2e802d1163ddaf3023c
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-05-18 23:52:17 +0200

    Updates

---
+++ precache-manifest.d11985c096d3f9d24e5d212baba5e397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88406ec32c262a3eb7b23dcde4b1bcc",
+    "url": "/WaysToGo/index.html"
+  },
+  {
+    "revision": "6cc9ffa84c8118840762",
+    "url": "/WaysToGo/static/css/main.6c68b9f7.chunk.css"
+  },
+  {
+    "revision": "b9bde06abe18d4aa46db",
+    "url": "/WaysToGo/static/js/2.3eb6383b.chunk.js"
+  },
+  {
+    "revision": "27f79e8878249bae55f598a0b8e19d20",
+    "url": "/WaysToGo/static/js/2.3eb6383b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc9ffa84c8118840762",
+    "url": "/WaysToGo/static/js/main.0b165422.chunk.js"
+  },
+  {
+    "revision": "f200946a1c0a8cd0965b",
+    "url": "/WaysToGo/static/js/runtime-main.29cc8039.js"
+  }
+]);
