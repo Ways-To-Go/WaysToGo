b007cc7b5119b656673c5a2291bae99bf9e79d93
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-05-19 18:32:07 +0200

    now retrieves user info

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
 import NavigationBar from "./NavigationBar";
+import Axios from "axios";
 
 export class Profile extends Component {
+  state = {
+    email: "",
+    firstname: "",
+    lastname: "",
+    id: "",
+  };
+
+  getUserInfo = () => {
+    // GET /api/me
+    Axios.get("https://wtg.aymerik-diebold.fr/api/me", {
+      headers: { Authorization: "Bearer " + this.props.token },
+    })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          email: res.data.email,
+          firstname: res.data.firstName,
+          lastname: res.data.lastName,
+          id: res.data.id,
+        });
+      })
+      .catch((err) => {
+        console.log("Not connected");
+        throw err;
+      });
+  };
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
   render() {
     return (
       <div className="profile">
@@ -9,8 +41,16 @@ export class Profile extends Component {
         <div className="profileContainer" style={containerStyle}>
           <h1>My Profile</h1>
           <div class="profileBox" style={boxStyle}>
-            <p class="left-profile">Email</p>
-            <p class="right-profile">john.doe</p>
+            <div>
+              <p class="left-profile">Email</p>
+              <p class="left-profile">Firstname</p>
+              <p class="left-profile">Lastname</p>
+            </div>
+            <div>
+              <p class="right-profile">{this.state.email}</p>
+              <p class="right-profile">{this.state.firstname}</p>
+              <p class="right-profile">{this.state.lastname}</p>
+            </div>
           </div>
         </div>
       </div>
