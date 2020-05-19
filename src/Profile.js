727f93b81296af5296daf8510f2bb6ffa83d9438
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-05-19 20:42:13 +0200

    added trips

--- src/Profile.js
+++ src/Profile.js
@@ -8,6 +8,8 @@ export class Profile extends Component {
     firstname: "",
     lastname: "",
     id: "",
+    trips: [],
+    tripTitles: [],
   };
 
   getUserInfo = () => {
@@ -22,12 +24,38 @@ export class Profile extends Component {
           firstname: res.data.firstName,
           lastname: res.data.lastName,
           id: res.data.id,
+          trips: res.data.trips,
         });
+        this.getTrips();
       })
       .catch((err) => {
         console.log("Not connected");
         throw err;
       });
+  };
+
+  getTrips = () => {
+    // GET /trips
+    console.log("get trips");
+    console.log(this.state.trips);
+    for (let i = 0; i < this.state.trips.length; i++) {
+      Axios.get(
+        "https://wtg.aymerik-diebold.fr/api/trips/" +
+          this.state.trips[i].split("/")[3]
+      )
+        .then((res) => {
+          console.log(res);
+          this.setState({
+            tripTitles: [
+              ...this.state.tripTitles,
+              { id: res.data.id, title: res.data.title },
+            ],
+          });
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
   };
 
   componentDidMount() {
@@ -40,6 +68,7 @@ export class Profile extends Component {
         <NavigationBar active="4" connected={this.props.connected} />
         <div className="profileContainer" style={containerStyle}>
           <h1>My Profile</h1>
+          <h3>Personal Info</h3>
           <div class="profileBox" style={boxStyle}>
             <div>
               <p class="left-profile">Email</p>
@@ -51,6 +80,14 @@ export class Profile extends Component {
               <p class="right-profile">{this.state.firstname}</p>
               <p class="right-profile">{this.state.lastname}</p>
             </div>
+          </div>
+          <div class="my-trips">
+            <h3>My trips</h3>
+            {this.state.tripTitles.map((trip) => (
+              <p>
+                <a href={"?show=trip&id=" + trip.id}>{trip.title}</a>
+              </p>
+            ))}
           </div>
         </div>
       </div>
